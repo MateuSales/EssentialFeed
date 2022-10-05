6c268ae13c5b8a2f2ca368b9f91e24eeff818baf
Mateus Sales <matheussalles40@gmail.com>
2022-10-04 23:42:12 -0300

    Delivers invalidData error on 200 HTTP response with valid json

--- EssentialFeedTests/RemoteFeedLoadTests.swift
+++ EssentialFeedTests/RemoteFeedLoadTests.swift
@@ -53,6 +53,18 @@ final class RemoteFeedLoadTests: XCTestCase {
             
             XCTAssertEqual(capturedErrors, [.invalidData])
         }
+        
+        func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+            let (sut, client) = makeSUT()
+            
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+
+            let invalidJSON = Data("invalid json".utf8)
+            client.complete(withStatusCode: 200, data: invalidJSON)
+            
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
     
     // MARK: - Helpers
@@ -78,7 +90,7 @@ final class RemoteFeedLoadTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: messages[index].url,
                 statusCode: code,
@@ -86,7 +98,7 @@ final class RemoteFeedLoadTests: XCTestCase {
                 headerFields: nil
             )!
             
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
 }
