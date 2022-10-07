30934e4826ccbd0b15229c7d84b1a27b959fe69d
Mateus Sales <matheussalles40@gmail.com>
2022-10-07 17:10:01 -0300

    Remove redundant test

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -20,17 +20,7 @@ class URLSessionHTTPClient {
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
-    
-    func test_getFromURL_createDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPClient(session: session)
         
-        sut.get(from: url)
-        
-        XCTAssertEqual(session.receivedURLs, [url])
-    }
-    
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
@@ -47,7 +37,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
     
     private class URLSessionSpy: URLSession {
-        private(set) var receivedURLs: [URL] = []
         private var stubs = [URL: URLSessionDataTask]()
         
         func stubs(url: URL, task: URLSessionDataTask) {
@@ -55,7 +44,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
 
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            receivedURLs.append(url)
             return stubs[url] ?? URLSessionDataTaskFake()
         }
     }
