9ff9a21e3367e2bb44a496b712136993590f9bd6
Mateus Sales <matheussalles40@gmail.com>
2022-10-16 01:28:08 -0300

    Load command delivers cached images on less than seven days old cache

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -13,7 +13,7 @@ public final class LocalFeedLoader {
     
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
-
+    
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
@@ -22,7 +22,7 @@ public final class LocalFeedLoader {
     public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed { [weak self] error in
             guard let self = self else { return }
-
+            
             if let cacheDeletionError = error {
                 completion(cacheDeletionError)
             } else {
@@ -32,11 +32,14 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { error in
-            if let error = error {
+        store.retrieve { result in
+            switch result {
+            case let .failure(error):
                 completion(.failure(error))
-            } else {
+            case .empty:
                 completion(.success([]))
+            case let .found(feed, _):
+                completion(.success(feed.toModels()))
             }
         }
     }
@@ -53,6 +56,19 @@ private extension Array where Element == FeedImage {
     func toLocal() -> [LocalFeedImage] {
         return map {
             LocalFeedImage(
+                id: $0.id,
+                description: $0.description,
+                location: $0.location,
+                url: $0.url
+            )
+        }
+    }
+}
+
+private extension Array where Element == LocalFeedImage {
+    func toModels() -> [FeedImage] {
+        return map {
+            FeedImage(
                 id: $0.id,
                 description: $0.description,
                 location: $0.location,
