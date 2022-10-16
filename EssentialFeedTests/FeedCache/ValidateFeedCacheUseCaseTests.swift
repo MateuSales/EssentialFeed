96a001bf51dd18e6c321f48f7347db32c13553d7
Mateus Sales <matheussalles40@gmail.com>
2022-10-16 12:37:31 -0300

    Extract cache deletion side-effect on retrieval error from the `load` method to be `validateCache` method

--- EssentialFeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
@@ -16,6 +16,15 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [])
     }
     
+    func test_validateCache_deletesCacheOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        
+        sut.validateCache()
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -24,5 +33,9 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
         trackForMemoryLeak(sut, file: file, line: line)
         trackForMemoryLeak(store, file: file, line: line)
         return (sut, store)
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 1)
     }
 }
